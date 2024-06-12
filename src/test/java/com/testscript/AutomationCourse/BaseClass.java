5733cdd588300264f004781dda672302961221ad
akhilv <akhilv@IGZ20039.cyspansys.com>
2024-06-12 16:29:40 +0530

    BaseClass commit

---
+++ src/test/java/com/testscript/AutomationCourse/BaseClass.java
@@ -0,0 +1,14 @@
+package com.testscript.AutomationCourse;
+
+public class BaseClass {
+	public void initializeBrowser()
+	{
+	}
+	
+            public static void main(String[] args) {
+			BaseClass base=new BaseClass();
+			base.initializeBrowser();
+			
+	}
+
+}
