9c86acfab5adbffd892033a2275021d9b71d87bc
akhilv <akhilv@IGZ20039.cyspansys.com>
2024-06-12 17:00:56 +0530

    Automation

---
+++ src/test/java/com/testscript/AutomationCourse/AutomationTest.java
@@ -0,0 +1,15 @@
+package com.testscript.AutomationCourse;
+
+public class AutomationTest {
+	public void initializeBrowser()
+	{
+		
+	}
+
+	public static void main(String[] args) {
+		
+		AutomationTest obj1=new AutomationTest();
+		obj1.initializeBrowser();
+	}
+
+}
